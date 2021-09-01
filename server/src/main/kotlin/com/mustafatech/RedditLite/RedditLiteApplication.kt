b741110d09dbd9a8fc10485bac51b9fb363efdf3
MustafaAli789 <alimustafa0987@gmail.com>
2021-09-01 17:41:31 -0400

    Server: Async email sending

--- server/src/main/kotlin/com/mustafatech/RedditLite/RedditLiteApplication.kt
+++ server/src/main/kotlin/com/mustafatech/RedditLite/RedditLiteApplication.kt
@@ -5,23 +5,11 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.JavaMailSenderImpl
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @SpringBootApplication
-class RedditLiteApplication {
-//	@Bean
-//	fun javaMailSender(): JavaMailSender {
-//		val mailSender = JavaMailSenderImpl()
-//		mailSender.host = "s***REMOVED***";
-//		mailSender.port = 25;
-//		mailSender.protocol = "SMTP"
-//
-//		mailSender.username = "***REMOVED***";
-//		mailSender.password = "***REMOVED***";
-//		return JavaMailSenderImpl()
-//	}
-}
-
-
+class RedditLiteApplication
 
 fun main(args: Array<String>) {
 	runApplication<RedditLiteApplication>(*args)
