e757677c10fec53548cfbe8081a1c92c26fc183c
MustafaAli789 <alimustafa0987@gmail.com>
2021-09-01 15:13:05 -0400

    Server: Mail service impl

---
+++ server/src/main/kotlin/com/mustafatech/RedditLite/service/MailService.kt
@@ -0,0 +1,34 @@
+package com.mustafatech.RedditLite.service
+
+import com.mustafatech.RedditLite.exception.SpringRedditException
+import com.mustafatech.RedditLite.model.NotificationEmail
+import org.slf4j.LoggerFactory
+import org.springframework.mail.MailException
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.mail.javamail.MimeMessagePreparator
+import org.springframework.stereotype.Service
+import javax.mail.event.MailEvent
+
+@Service
+class MailService(val mailSender: JavaMailSender, val mailContentBuilder: MailContentBuilder) {
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    fun sendMail(notificationEmail: NotificationEmail) {
+        val msgPreparator = MimeMessagePreparator { mimeMessage ->
+            val msgHelper = MimeMessageHelper(mimeMessage)
+            msgHelper.setFrom("springreddit@email.com")
+            msgHelper.setTo(notificationEmail.recipient)
+            msgHelper.setSubject(notificationEmail.subject)
+            msgHelper.setText(mailContentBuilder.build(notificationEmail.body))
+        }
+        try {
+            mailSender.send(msgPreparator)
+            logger.info("Activation email sent")
+        } catch (e: MailException) {
+            throw SpringRedditException("Exception occurred when sending mail to ${notificationEmail.subject}")
+        }
+    }
+
+}
