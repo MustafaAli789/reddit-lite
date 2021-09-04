efca3435a333a72d7aee5a9e37b92e39f5592c31
MustafaAli789 <alimustafa0987@gmail.com>
2021-09-03 23:00:10 -0400

    Server: cors configuration

--- server/src/main/kotlin/com/mustafatech/RedditLite/config/AppConfig.kt
+++ server/src/main/kotlin/com/mustafatech/RedditLite/config/AppConfig.kt
@@ -4,9 +4,23 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class AppConfig {
+@EnableWebMvc
+class AppConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
