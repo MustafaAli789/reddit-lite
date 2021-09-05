949af084a31e6cf92722c07b090699c84dc72e78
MustafaAli789 <alimustafa0987@gmail.com>
2021-09-04 21:15:14 -0400

    Cleint: Header and home component fixes and post component+list subreddit components creation

--- client/reddit-lite-ang/src/app/app-routing.module.ts
+++ client/reddit-lite-ang/src/app/app-routing.module.ts
@@ -5,8 +5,13 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  {path: 'signup', component: SignupComponent},
+  { path: '', component: HomeComponent },
+  { path: 'view-post/:id', component: ViewPostComponent },
+  { path: 'user-profile/:name', component: UserProfileComponent, canActivate: [AuthGuard] },
+  { path: 'list-subreddits', component: ListSubredditsComponent },
+  { path: 'create-post', component: CreatePostComponent, canActivate: [AuthGuard] },
+  { path: 'create-subreddit', component: CreateSubredditComponent, canActivate: [AuthGuard] },
+  { path: 'sign-up', component: SignupComponent },
   { path: 'login', component: LoginComponent }
 ];
 
