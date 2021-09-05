29c3bf52ae25a9d4b1721e2096697e210d4e6a74
MustafaAli789 <alimustafa0987@gmail.com>
2021-09-04 20:49:32 -0400

    Client: Vote component impl update

--- client/reddit-lite-ang/src/app/shared/vote-button/vote-button.component.ts
+++ client/reddit-lite-ang/src/app/shared/vote-button/vote-button.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PostModel } from '../post-model';
+import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { VotePayload } from './vote-payload';
+import { VoteType } from './vote-type';
+import { VoteService } from '../vote.service';
+import { AuthService } from 'src/app/auth/shared/auth.service';
+import { PostService } from '../post.service';
+import { throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-vote-button',
@@ -7,9 +16,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteButtonComponent implements OnInit {
 
-  constructor() { }
+  @Input() post: PostModel = {};
+  votePayload: VotePayload;
+  faArrowUp = faArrowUp;
+  faArrowDown = faArrowDown;
+  upvoteColor?: string;
+  downvoteColor?: string;
+  isLoggedIn?: boolean;
 
-  ngOnInit(): void {
+  constructor(private voteService: VoteService,
+    private authService: AuthService,
+    private postService: PostService, private toastr: ToastrService) {
+
+    this.votePayload = {
+      voteType: undefined,
+      postId: undefined
+    }
+    this.authService.loggedIn.subscribe((data: boolean) => this.isLoggedIn = data);
   }
 
+  ngOnInit(): void {
+    this.updateVoteDetails();
+  }
+
+  upvotePost() {
+    this.votePayload.voteType = VoteType.UPVOTE;
+    this.vote();
+    this.downvoteColor = '';
+  }
+
+  downvotePost() {
+    this.votePayload.voteType = VoteType.DOWNVOTE;
+    this.vote();
+    this.upvoteColor = '';
+  }
+
+  private vote() {
+    this.votePayload.postId = this.post?.id;
+    this.voteService.vote(this.votePayload).subscribe(() => {
+      this.updateVoteDetails();
+    }, (error: { error: { message: string | undefined; }; }) => {
+      this.toastr.error(error.error.message);
+      throwError(error);
+    });
+  }
+
+  private updateVoteDetails() {
+    this.postService.getPost(this.post.id ? this.post.id : 0).subscribe(post => {
+      this.post = post;
+    });
+  }
 }
---
+++ client/reddit-lite-ang/src/app/subreddit/list-subreddit/list-subreddit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-subreddit',
+  templateUrl: './list-subreddit.component.html',
+  styleUrls: ['./list-subreddit.component.css']
+})
+export class ListSubredditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
